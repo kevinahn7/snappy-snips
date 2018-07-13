a9164c3889255efa8b6d3170b4e3ab25d08d6ff9
Kevin Ahn <kevinahn7@hotmail.com>
2018-07-13 11:08:30 -0700

    Add GetAll method to Client class

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace HairSalon.Models
 {
@@ -31,6 +32,44 @@ namespace HairSalon.Models
             }
         }
 
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
 
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM clients;";
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public static List<Client> GetAll()
+        {
+            List<Client> allClient = new List<Client> { };
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM people;";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int id = rdr.GetInt32(0);
+                string name = rdr.GetString(1);
+                Client newClient = new Client(name, id);
+                allClient.Add(newClient);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allClient;
+        }
     }
 }
