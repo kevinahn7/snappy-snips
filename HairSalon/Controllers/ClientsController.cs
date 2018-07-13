e6d12a90facf431d36383a9290b242243b64d0ec
Kevin Ahn <kevinahn7@hotmail.com>
2018-07-13 15:19:21 -0700

    Add clients controller and view

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
     public class ClientsController : Controller
     {
-        
+        [HttpGet("/clients")]
         public ActionResult Index()
         {
-            return View();
+            List<Stylist> allStylists = Stylist.GetAll();
+            return View(allStylists);
+        }
+
+        [HttpPost("/clients")]
+        public ActionResult Index(string name, int stylistId)
+        {
+            Client newClient = new Client(name, stylistId);
+            newClient.Save();
+            return RedirectToAction("Index");
         }
     }
 }
