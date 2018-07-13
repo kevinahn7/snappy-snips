4a2f6e4bafa535a405b234475dfd9b77429db9d9
Kevin Ahn <kevinahn7@hotmail.com>
2018-07-13 10:36:46 -0700

    Add database model and mysqlconnection

---
+++ HairSalon/Models/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using HairSalon;
+
+namespace HairSalon.Models
+{
+    public class DB
+    {
+        public static MySqlConnection Connection()
+        {
+            MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+            return conn;
+        }
+    }
+}
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,5 +7,22 @@ namespace HairSalon.Models
 {
     public class Stylist
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public int ClientId { get; set; }
+
+        public Stylist(int id, string name, string details, int clientId)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Details = details;
+            this.ClientId = clientId;
+        }
+
+
+
+
+
     }
 }
