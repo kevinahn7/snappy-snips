253e6119573b09e7173d6d956495ba1bd4d2d362
Kevin Ahn <kevinahn7@hotmail.com>
2018-07-13 11:05:51 -0700

    Add DeleteAll method to Client class

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -7,5 +7,30 @@ namespace HairSalon.Models
 {
     public class Client
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Client(string name, int id = 0)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public override bool Equals(System.Object otherClient)
+        {
+            if (!(otherClient is Client))
+            {
+                return false;
+            }
+            else
+            {
+                Client newClient = (Client)otherClient;
+                bool idEquality = (this.Id == newClient.Id);
+                bool nameEquality = (this.Name == newClient.Name);
+                return (idEquality && nameEquality);
+            }
+        }
+
+
     }
 }
