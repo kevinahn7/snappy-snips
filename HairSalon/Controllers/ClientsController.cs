804aaaed59251eaf8fd9caefa26b5f2a548dec97
Kevin Ahn <kevinahn7@hotmail.com>
2018-07-13 13:25:51 -0700

    Add ClientsController and StylistsController

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
-    public class HomeController : Controller
+    public class ClientsController : Controller
     {
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
