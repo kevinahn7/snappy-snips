52e15eaa81ab1b1d3b2f7db80a133ef8127aa1d0
Kevin Ahn <kevinahn7@hotmail.com>
2018-07-20 09:42:38 -0700

    Add Specialty class in model

---
+++ HairSalon/Models/Specialty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HairSalon.Models
+{
+    public class Specialty
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Specialty(string name, int id = 0)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
