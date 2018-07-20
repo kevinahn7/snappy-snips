551ef4a59c3c043f13ed903faf0f9b034fbcd155
Kevin Ahn <kevinahn7@hotmail.com>
2018-07-20 10:57:57 -0700

    Add specialty controllers and views

---
+++ HairSalon/Controllers/SpecialtiesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+    public class SpecialtiesController : Controller
+    {
+        [HttpGet("/specialties")]
+        public ActionResult Index()
+        {
+            List<Specialty> listOfSpecialties = Specialty.GetAll();
+            return View(listOfSpecialties);
+        }
+
+        [HttpPost("/specialties")]
+        public ActionResult Index(string name)
+        {
+            Specialty newSpecialty = new Specialty(name);
+            newSpecialty.Save();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("/specialties/add")]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpGet("/specialties/{id}")]
+        public ActionResult Details(int id)
+        {
+            Specialty chosenSpecialty = Specialty.Find(id);
+            return View(chosenSpecialty);
+        }
+
+        [HttpGet("/specialties/{id}/delete")]
+        public ActionResult Delete(int id)
+        {
+            Specialty.DeleteSingleSpecialty(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
