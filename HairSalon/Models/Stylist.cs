b16fb6b8befcbf62a7f8309aaca007257ffd1550
Kevin Ahn <kevinahn7@hotmail.com>
2018-07-20 10:36:42 -0700

    Add AddSpecialty and GetSpecialties methods to Stylist class

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -40,7 +40,7 @@ namespace HairSalon.Models
             conn.Open();
 
             var cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"DELETE FROM stylists;";
+            cmd.CommandText = @"DELETE FROM stylists; DELETE FROM stylists_specialties;";
 
             cmd.ExecuteNonQuery();
 
@@ -160,7 +160,7 @@ namespace HairSalon.Models
             MySqlConnection conn = DB.Connection();
             conn.Open();
             var cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"DELETE FROM stylists WHERE id = " + id + ";";
+            cmd.CommandText = @"DELETE FROM stylists WHERE id = " + id + "; DELETE FROM stylists_specialties WHERE stylist_id = " + id + ";";
 
             cmd.ExecuteNonQuery();
 
@@ -169,6 +169,69 @@ namespace HairSalon.Models
             {
                 conn.Dispose();
             }
+        }
+
+        public void addSpecialty(Specialty newSpecialty)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO stylists_specialties (stylist_id, specialty_id) VALUES (@StylistId, @SpecialtyId);";
+
+            cmd.Parameters.AddWithValue("@StylistId", Id);
+            cmd.Parameters.AddWithValue("@SpecialtyId", newSpecialty.Id);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public List<Specialty> GetSpecialties()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT specialty_id FROM stylists_specialties WHERE stylist_id = @StylistId;";
+
+            cmd.Parameters.AddWithValue("@StylistId", Id);
+
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+            List<int> specialtyIds = new List<int> { };
+            while (rdr.Read())
+            {
+                int specialtyId = rdr.GetInt32(0);
+                specialtyIds.Add(specialtyId);
+            }
+            rdr.Dispose();
+
+            List<Specialty> specialties = new List<Specialty> { };
+            foreach (int specialtyId in specialtyIds)
+            {
+                var specialtyQuery = conn.CreateCommand() as MySqlCommand;
+                specialtyQuery.CommandText = @"SELECT * FROM specialties WHERE id = @SpecialtyId;";
+
+                cmd.Parameters.AddWithValue("@SpecialtyId", Id);
+
+                var specialtyQueryRdr = specialtyQuery.ExecuteReader() as MySqlDataReader;
+                while (specialtyQueryRdr.Read())
+                {
+                    int thisSpecialtyId = specialtyQueryRdr.GetInt32(0);
+                    string specialtyName = specialtyQueryRdr.GetString(1);
+                    Specialty foundSpecialty = new Specialty(specialtyName, thisSpecialtyId);
+                    specialties.Add(foundSpecialty);
+                }
+                specialtyQueryRdr.Dispose();
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return specialties;
         }
 
     }
